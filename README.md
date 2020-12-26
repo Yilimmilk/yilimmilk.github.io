2acb08488d2b7a7d6aabb38308919f456246ef33
Yilimmilk <yi18827360220@gmail.com>
2020-12-26 22:22:14 +0800

    重写了主页

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # yilimmilk.github.io
-Yilimmilk的个人主页
 
-基于[Dmego](https://github.com/dmego/home.github.io)的项目修改
-过段时间会大修改的，现在没啥空~
+源码来自<https://github.com/laoxuai/home> 在这基础上进行的修改
--- assets/js/main.js
+++ assets/js/main.js
@@ -38,8 +38,8 @@ $(document).ready(function () {
 		console.error(err);
 	})
 
-	
-	// var url = 'https://query.yahooapis.com/v1/public/yql' + 
+
+	// var url = 'https://query.yahooapis.com/v1/public/yql' +
     // '?q=' + encodeURIComponent('select * from json where url=@url') +
     // '&url=' + encodeURIComponent('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8') +
 	// '&format=json&callback=?';
@@ -48,15 +48,15 @@ $(document).ready(function () {
 	 * 获取Bing壁纸
 	 * 原先 YQL 已经无法提供服务了
 	 * 改用 JsonBird：https://bird.ioliu.cn/
-	 * 
+	 *
 	 */
 	var url = 'https://bird.ioliu.cn/v1/?url=https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8';
 	var imgUrls = JSON.parse(sessionStorage.getItem("imgUrls"));
 	var index = sessionStorage.getItem("index");
-	var $panel = $('#panel');
+	var $panel = $('#nav');
 	if(imgUrls == null){
 		imgUrls = new Array();
-		index = 0;		
+		index = 0;
 		$.get(url,function (result) {
 			images = result.images;
 			for (let i = 0; i < images.length; i++) {
@@ -81,26 +81,62 @@ $(document).ready(function () {
 		$panel.css("background-size", "cover");
 		sessionStorage.setItem("index",index);
 	}
-	
+
 	$(".iUp").each(function (i, e) {
 		iUp.up(e);
 	});
-
-	$(".js-avatar")[0].onload = function () {
-		$(".js-avatar").addClass("show");
-	}
 });
 
-$('.btn-mobile-menu__icon').click(function() {
-    if ($('.navigation-wrapper').css('display') == "block") {
-      $('.navigation-wrapper').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-        $('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
-        $('.navigation-wrapper').off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
-      });
-      $('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
+console.clear();
+console.log("%c 你想干啥 Σ( ° △ °|||)︴.", "background:#24272A; color:#ffffff", "");
+console.log("%c Github %c", "background:#24272A; color:#ffffff", "", "https://github.com/Yilimmilk");
+console.log("%c 版本号: %c", "background:#24272A; color:#ffffff", "", "2.0.1");
 
-    } else {
-      $('.navigation-wrapper').toggleClass('visible animated bounceInDown');
-    }
-    $('.btn-mobile-menu__icon').toggleClass('social iconfont icon-list social iconfont icon-angleup animated fadeIn');
+
+(function($) {
+  $.fn.snow = function(options) {
+    var $flake = $('<div id="snowbox" />').css({
+        'position': 'absolute',
+        'z-index': '9999',
+        'top': '-50px'
+      }).html('&#10052;'),
+      documentHeight = $(document).height(),
+      documentWidth = $(document).width(),
+      defaults = {
+        minSize: 10,
+        maxSize: 20,
+        newOn: 1000,
+        flakeColor: "#AFDAEF" /* 此处可以定义雪花颜色，若要白色可以改为#FFFFFF */
+      },
+      options = $.extend({}, defaults, options);
+    var interval = setInterval(function() {
+      var startPositionLeft = Math.random() * documentWidth - 100,
+        startOpacity = 0.5 + Math.random(),
+        sizeFlake = options.minSize + Math.random() * options.maxSize,
+        endPositionTop = documentHeight - 200,
+        endPositionLeft = startPositionLeft - 500 + Math.random() * 500,
+        durationFall = documentHeight * 10 + Math.random() * 5000;
+      $flake.clone().appendTo('body').css({
+        left: startPositionLeft,
+        opacity: startOpacity,
+        'font-size': sizeFlake,
+        color: options.flakeColor
+      }).animate({
+        top: endPositionTop,
+        left: endPositionLeft,
+        opacity: 0.2
+      }, durationFall, 'linear', function() {
+        $(this).remove()
+      });
+    }, options.newOn);
+  };
+})(jQuery);
+$(function() {
+  $.fn.snow({
+    minSize: 5,
+    /* 定义雪花最小尺寸 */
+    maxSize: 20,
+    /* 定义雪花最大尺寸 */
+    newOn: 800 /* 定义密集程度，数字越小越密集 */
+  });
 });
